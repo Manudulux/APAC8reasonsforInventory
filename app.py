3bc867c980608ae552040b1e20e5a1fdd8743b4a
Manudulux <51920504+Manudulux@users.noreply.github.com>
2026-04-30 08:22:18 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -691,8 +691,8 @@ elif page == "📊 Dashboard 1":
             fig_c.add_annotation(
                 xref="paper", yref="paper", x=0, y=1.05,
                 text="Consumption Qty (MT)", showarrow=False,
-                font=dict(size=10, color="#1e3a7b"), bgcolor="#1e3a7b",
-                fontcolor="white", borderpad=4
+                font=dict(size=10, color="white"), bgcolor="#1e3a7b",
+                borderpad=4
             )
             st.plotly_chart(fig_c, use_container_width=True)
         else:
